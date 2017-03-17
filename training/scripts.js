00647b024699f73798925e9c9bace63e59067f35
Tiago Peres <tiagomartinsperes@gmail.com>
2017-03-17 12:16:12 +0000

    some work using [JavaScript]
    Contact me for more information:
    
    tiagomartinsperes@gmail.com (Email)
    spastnudel(GitHub)
    tiagomartinsperes(Instagram)
    tiago-peres(LinkedIn)
    tiagoperes (Reddit)
    tiago.martins.peres(Skype)
    tiago_peres(SoundCloud)
    tiagoperes(VK)
    tiago-peres(WeChat)
    http://tiagoperes.wpgeekdevs.com/ (Website)

---
+++ training/scripts.js
@@ -0,0 +1,61 @@
+    function getRandomColor() {
+        var letters = '0123456789ABCDEF'.split('');
+        var color = '#';
+        for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+        }
+        return color;
+    }
+      
+    var clickedTime;  
+    var createdTime;
+    var reactionTime
+        
+    function makeBox() {
+            
+        var time= Math.random();
+        time = time * 5000;
+     
+        setTimeout(function() {
+        
+        if (Math.random()>0.5) {
+        
+            document.getElementById("box").style.borderRadius="100px";
+        
+        } else {
+        
+            document.getElementById("box").style.borderRadius="0";
+        
+        }
+        
+        var top= Math.random();
+        var left= Math.random();
+        top= top*300;
+        left= left*1000;
+        
+        document.getElementById("box").style.top= top +"px";
+        
+        document.getElementById("box").style.left= left + "px";
+        
+        document.getElementById("box").style.backgroundColor= getRandomColor();
+     
+        document.getElementById("box").style.display= "block";
+        
+        createdTime= Date.now();
+        
+        }, time);
+     }
+        document.getElementById("box").onclick= function() {
+     
+        clickedTime= Date.now();
+        
+        reactionTime= (clickedTime-createdTime)/1000; //gives the time in seconds if divide ms per 1000
+        
+        document.getElementById("time").innerHTML=reactionTime;
+        
+        this.style.display= "none";
+     
+        makeBox();
+        }
+     
+    makeBox();
